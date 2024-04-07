1c96aaaa22c3c7ed1aeb6b8329dd01ee917ab76a
César Raúl Landa Aponte <cr.landaa@gmail.com>
2024-04-07 12:05:43 -0700

    resumir perdida SNI

--- 01-clean.R
+++ 01-clean.R
@@ -14,3 +14,16 @@ dir.create(path = "01.results")
 # guardar date
 write.csv(x = data_filtrada, file = "01.results/limpia.csv")
 
+# CAlcular la proporcion de doctores que perdieron el SNI
+
+data_resumen <- data_filtrada %>%
+                filter(cambio == "perdio SNI")%>%
+                group_by(nobilis) %>% 
+                summarise(totales = sum(miembros))
+
+write.csv(x = data_resumen, file = "01.results/resumen.csv")
+
+
+# TO-DOs
+# clanda: calcular el % de DR y DRA que pierde SNI
+#            como total ingresos/ total perdidas
